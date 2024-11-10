986997b9fb1b2d1401b885020ca169be2b4b758d
Jose Luis Izquierdo Hernandez <110314046+jocl0110@users.noreply.github.com>
2024-11-10 17:03:46 -0600

    Implemented the navigation to the productDetails component

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,7 +1,75 @@
+import { useEffect, useState } from "react";
+import { IoStar } from "react-icons/io5";
+import { useParams } from "react-router-dom";
+
 const ProductDetails = () => {
+  const { id } = useParams();
+
+  const [productDetails, setProductDetails] = useState({});
+
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      const response = await fetch(`https://dummyjson.com/products/${id}`);
+      const data = await response.json();
+      setProductDetails(data);
+      console.log(data);
+    };
+    fetchProductDetails();
+  }, [id]);
+
   return (
     <div>
-      <h2>Product Details</h2>
+      <div className="route"></div>
+      <div>
+        <img
+          style={{ width: "auto" }}
+          src={productDetails.thumbnail}
+          alt={productDetails.title}
+        />
+        <p>
+          <IoStar />
+          {productDetails.rating}
+        </p>
+        <h3>{productDetails.title}</h3>
+        <p>${productDetails.price}</p>
+        <button>Add to Cart</button>
+        <div className="about">
+          <div>
+            About this Item
+            <div>
+              Specifications
+              <div>
+                <ul>
+                  {productDetails.dimensions ? (
+                    Object.entries(productDetails.dimensions).map(
+                      ([key, value]) => (
+                        <li key={key}>
+                          <strong>{key}</strong>: {value} inches
+                        </li>
+                      )
+                    )
+                  ) : (
+                    <p>No product details</p>
+                  )}
+                </ul>
+              </div>
+            </div>
+            <div>
+              Description
+              <p>{productDetails.description}</p>
+            </div>
+            <div>
+              Warranty Information
+              <p>{productDetails.warrantyInformation}</p>
+            </div>
+            <div>
+              Shiping and Return
+              <p>{productDetails.shippingInformation}</p>
+              <p>{productDetails.returnPolicy}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
